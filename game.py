950a0084b4a97216a704edacbbf2cc3b2c9f8f4e
ajaymittur28 <ajay.cs18@bmsce.ac.in>
2019-07-16 17:31:58 +0530

    added game over screen

--- game.py
+++ game.py
@@ -10,22 +10,57 @@ screen_size = screen_width, screen_height = 820, 820
 black = 0, 0, 0
 white = 255, 255, 255
 speed = [0, 0]
+font_size = 36
+margin = 100
 
 screen = pygame.display.set_mode(screen_size)
-fruit = Fruit(screen_width, screen_height)
-fruit_rect = fruit.get_rect()
-snake = Snake(screen_width, screen_height)
-snake_body = snake.get_body()
-fruit_eaten = False
-eaten_fruit_position = 0, 0
+clock = pygame.time.Clock()
+font = pygame.font.Font(None, font_size)
+
+def gameInit():
+    # make variables global and make function access global variables
+    global fruit, fruit_rect, snake, snake_body, fruit_eaten, eaten_fruit_position
+
+    fruit = Fruit(screen_width, screen_height)
+    fruit_rect = fruit.get_rect()
+    snake = Snake(screen_width, screen_height)
+    snake_body = snake.get_body()
+    fruit_eaten = False
+    eaten_fruit_position = 0, 0
+
+    print('Game Initialized')
 
 def check_boundaries(head):
     if head.left < 0 or head.right > screen_width or head.top < 0 or head.bottom > screen_height:
-        sys.exit()
+        gameOver()
 
 def check_bodyhead_collision(body):
     if body[0].collidelist(body[1:]) != -1:
-        sys.exit()
+        gameOver()
+
+def gameOver():
+    print('Game Over')
+    screen.fill(white)
+    displayMessage('Game Over', center=(screen_width/2, screen_height/2 - margin))
+    displayMessage('[q] to quit', left=200, top=screen_height/2)
+    displayMessage('[r] to restart', right=620, top=screen_height/2)
+    pygame.display.update()
+
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE or event.key == pygame.K_q:
+                    sys.exit()
+                elif event.key == pygame.K_r:
+                    gameInit()
+                    return
+
+def displayMessage(text, color=black, background=white, **position_args):
+    text_surface = font.render(text, True, color, background)
+    text_rect = text_surface.get_rect(**position_args)
+    screen.blit(text_surface, text_rect)
+
+gameInit()
 
 while 1:
     for event in pygame.event.get():
@@ -63,5 +98,7 @@ while 1:
             snake_body = snake.get_body()
             fruit_eaten = False
 
-        pygame.display.flip()
-        pygame.time.delay(100)
+        pygame.display.update()
+        clock.tick(10)
+        # same as above ie. 10 frames per second
+        # pygame.time.delay(100)
