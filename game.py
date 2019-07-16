902c1362751ec0591bd93d96bbfab55fbc3be957
ajaymittur28 <ajay.cs18@bmsce.ac.in>
2019-07-16 23:55:46 +0530

    added scoreboard

--- game.py
+++ game.py
@@ -12,6 +12,7 @@ white = 255, 255, 255
 speed = [0, 0]
 font_size = 36
 margin = 100
+HIDESCORE = pygame.USEREVENT + 1
 
 screen = pygame.display.set_mode(screen_size)
 clock = pygame.time.Clock()
@@ -19,7 +20,15 @@ font = pygame.font.Font(None, font_size)
 
 def gameInit():
     # make variables global and make function access global variables
-    global fruit, fruit_rect, snake, snake_body, fruit_eaten, eaten_fruit_position
+    global fruit
+    global fruit_rect 
+    global snake 
+    global snake_body 
+    global fruit_eaten 
+    global eaten_fruit_position
+    global score
+    global display_score
+    global set_timer
 
     fruit = Fruit(screen_width, screen_height)
     fruit_rect = fruit.get_rect()
@@ -27,27 +36,36 @@ def gameInit():
     snake_body = snake.get_body()
     fruit_eaten = False
     eaten_fruit_position = 0, 0
+    score = 0
+    display_score = False
+    set_timer = False
 
     print('Game Initialized')
+
 
 def check_boundaries(head):
     if head.left < 0 or head.right > screen_width or head.top < 0 or head.bottom > screen_height:
         gameOver()
 
+
 def check_bodyhead_collision(body):
     if body[0].collidelist(body[1:]) != -1:
         gameOver()
 
+
 def gameOver():
-    print('Game Over')
     screen.fill(white)
-    displayMessage('Game Over', center=(screen_width/2, screen_height/2 - margin))
+    displayMessage('Game Over', center=(screen_width/2, screen_height/2 - 2*margin))
+    displayMessage(f'Final Score: {score}', center=(screen_width/2, screen_height/2 - margin))
     displayMessage('[q] to quit', left=200, top=screen_height/2)
     displayMessage('[r] to restart', right=620, top=screen_height/2)
     pygame.display.update()
+    print('Game Over')
 
     while 1:
         for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE or event.key == pygame.K_q:
                     sys.exit()
@@ -55,13 +73,20 @@ def gameOver():
                     gameInit()
                     return
 
+
 def displayMessage(text, color=black, background=white, **position_args):
     text_surface = font.render(text, True, color, background)
     text_rect = text_surface.get_rect(**position_args)
     screen.blit(text_surface, text_rect)
 
-gameInit()
 
+def displayScore(score, set_timer):
+    displayMessage(f'Score: {score}', white, black, centerx=screen_width/2, centery=margin)
+    if set_timer:
+        pygame.time.set_timer(HIDESCORE, 2000)
+
+
+gameInit()
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -77,6 +102,9 @@ while 1:
                 snake.set_speed([0, 20])
             if event.key == pygame.K_UP:
                 snake.set_speed([0, -20])
+        if event.type == HIDESCORE:
+            display_score = False
+            pygame.time.set_timer(HIDESCORE, 0)
 
     if pygame.key.get_focused():
         snake.move_head()
@@ -92,11 +120,18 @@ while 1:
             fruit_eaten = True
             eaten_fruit_position = fruit_rect.x, fruit_rect.y
             fruit_rect = fruit.change_pos(screen_width, screen_height)
+            score += 1
+            display_score = True
+            set_timer = True
 
         if fruit_eaten and (snake_body[-1].x, snake_body[-1].y) == eaten_fruit_position:
             snake.add_body(eaten_fruit_position)
             snake_body = snake.get_body()
             fruit_eaten = False
+        
+        if display_score:
+            displayScore(score, set_timer)
+            set_timer = False
 
         pygame.display.update()
         clock.tick(10)
