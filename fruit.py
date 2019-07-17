b7557a9eb7a22a9897c179cd7d7af4d484daf195
ajaymittur28 <ajay.cs18@bmsce.ac.in>
2019-07-18 00:14:44 +0530

    eliminated possibility of fruit spawning in the snake

--- fruit.py
+++ fruit.py
@@ -9,9 +9,14 @@ class Fruit:
         self.position_y = random.randrange(0, screen_height, self.height)
         self.fruit_rect = pygame.Rect((0, 0), (self.size))
 
-    def change_pos(self, screen_width, screen_height):
+    def change_pos(self, screen_width, screen_height, snake_body_coords):
         self.position_x = random.randrange(0, screen_width, self.width)
         self.position_y = random.randrange(0, screen_height, self.height)
+
+        # make sure fruit doesn't spawn in snake body
+        if (self.position_x, self.position_y) in snake_body_coords:
+            self.position_x = random.randrange(0, screen_width, self.width)
+            self.position_y = random.randrange(0, screen_height, self.height)
         return self.get_rect()
     
     def get_rect(self):
